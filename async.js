14545a0fe4765af8b73552d115c222d1feba7163
spt30 <seregatoschilin@gmail.com>
2017-11-30 04:08:42 +0500

    эмммм

--- async.js
+++ async.js
@@ -26,41 +26,51 @@ function runParallel(jobs, parallelNum, timeout = 1000) {
     }
 
     function startJob(innerCounter, resolve) {
-        console.info('q', firstCounter);
+        // console.info('q', firstCounter);
         // firstCounter++;
         // let innerCounter = firstCounter++;
-        console.info(innerCounter);
-        console.info(firstCounter);
+        // console.info(innerCounter);
+        // console.info(firstCounter);
         const resEssence = jobStartToFinish => finishJob(jobStartToFinish, innerCounter, resolve);
         // const resEssence = (jobStartToFinish) => finishJob(jobStartToFinish, innerCounter++);
         // const resEssence = (jobStartToFinish) => finishJob(jobStartToFinish, ++innerCounter);
-        console.info(innerCounter);
-        console.info('q', firstCounter);
+        // console.info(innerCounter);
+        // console.info('q', firstCounter);
         // firstCounter++;
         currentElement(innerCounter)
             .then(resEssence)
             .catch(resEssence);
     }
     function finishJob(jobFromStart, innerCounter, resolve) {
-        console.info('FINISHF', innerCounter);
-        console.info('FINISHS', secondCounter);
+        // console.info('FINISHF', innerCounter);
+        // console.info('FINISHS', secondCounter);
         result[innerCounter] = jobFromStart;
-        switch (jobs.length) {
-            case ++secondCounter:
-                resolve(result);
-                // console.info('FINISHF2', innerCounter);
-                // console.info('FINISHS2', secondCounter);
-                break;
-            case firstCounter:
-                break;
-            default:
-                startJob(firstCounter++, resolve);
-                // console.info('FINISHF3', innerCounter);
-                // console.info('FINISHS3', secondCounter);
+        if (jobs.length === ++secondCounter) {
+            // console.info('CASE1', jobs.length );
+            resolve(result);
         }
+
+        if (firstCounter < jobs.length) {
+            // console.info('CASE2', jobs.length );
+            startJob(firstCounter++, resolve);
+        }
+        // switch (jobs.length) {
+        //     case ++secondCounter:
+        //         resolve(result);
+        //         // console.info('FINISHF2', innerCounter);
+        //         // console.info('FINISHS2', secondCounter);
+        //         break;
+        //     case firstCounter:
+        //         break;
+        //     default:
+        //         startJob(firstCounter++, resolve);
+        //         // console.info('FINISHF3', innerCounter);
+        //         // console.info('FINISHS3', secondCounter);
+        // }
     }
+
     return new Promise(resolve => {
-        if (!jobs.length) {
+        if (parallelNum <= 0 || !jobs.length) {
             resolve(result);
         }
         while (firstCounter < parallelNum) {
